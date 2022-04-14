269813d8be81cb349cd15a4dad2fb0dafd767417
Enrico Rotundo <enricorotundo@Enricos-MBP.fritz.box>
2022-04-14 15:13:50 +0200

    up

--- benchmark/word-count/spark/run.sh
+++ benchmark/word-count/spark/run.sh
@@ -5,18 +5,14 @@ set -euxo pipefail
 
 
 # launch job
-if [ ${DATASET_NAME} = "wordcountTiny" ]; then
-  if [ ${EXP_NAME} = "cluster-size-1" ]; then
-    $SPARK_HOME/bin/spark-submit \
+if [ ${EXP_NAME} = "cluster-size-1" ]; then
+  $SPARK_HOME/bin/spark-submit \
       --master local[*] \
       word-count/spark/target/scala-2.12/sparkwordcount_2.12-0.1.jar \
       ${DATASET_LOCATION}
-    else
-      $SPARK_HOME/bin/spark-submit \
-        --master spark://hadoop-master:7077 \
-        word-count/spark/target/scala-2.12/sparkwordcount_2.12-0.1.jar \
-        ${DATASET_LOCATION}
-    fi
 else
-  echo "try again"
+  $SPARK_HOME/bin/spark-submit \
+      --master spark://hadoop-master:7077 \
+      word-count/spark/target/scala-2.12/sparkwordcount_2.12-0.1.jar \
+      ${DATASET_LOCATION}
 fi
