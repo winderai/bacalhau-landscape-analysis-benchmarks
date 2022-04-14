189ab9686cd6fbf92b9b9d9d7439ffc057017453
Enrico Rotundo <enricorotundo@Enricos-MBP.fritz.box>
2022-04-14 15:11:14 +0200

    up

--- benchmark/word-count/hadoop/run.sh
+++ benchmark/word-count/hadoop/run.sh
@@ -4,8 +4,4 @@
 set -euxo pipefail
 
 # launch job
-if [ ${DATASET_NAME} = "wordcountTiny" ]; then
-    $HADOOP_HOME/bin/yarn jar word-count/hadoop/wc.jar WordCount ${DATASET_LOCATION} out
-else
-  echo "try again"
-fi
+$HADOOP_HOME/bin/yarn jar word-count/hadoop/wc.jar WordCount ${DATASET_LOCATION} out
