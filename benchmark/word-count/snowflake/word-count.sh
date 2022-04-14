cd2f79b3d2e2157dfe0fc02eda529660ba3ffaa6
Enrico Rotundo <enricorotundo@Enricos-MBP.fritz.box>
2022-04-14 15:34:32 +0200

    up

--- benchmark/word-count/snowflake/word-count.sh
+++ benchmark/word-count/snowflake/word-count.sh
@@ -4,15 +4,11 @@
 set -euxo pipefail
 
 
-if [ ${DATASET_NAME} = "wordcountTiny" ]; then
-    /home/ubuntu/bin/snowsql --query "SELECT word, COUNT(*) as count
-      from (
-        select c.value::string as word 
-        from "${SNOW_DBNAME}"."${SNOW_SCHEMANAME}"."${DATASET_NAME}", 
-              lateral flatten(input=>split(C1, ' ')) c
-      )
-      group by word
-      limit 10;"
-else
-  echo "try again"
-fi
+/home/ubuntu/bin/snowsql --query "SELECT word, COUNT(*) as count
+  from (
+    select c.value::string as word 
+    from "${SNOW_DBNAME}"."${SNOW_SCHEMANAME}"."${DATASET_NAME}", 
+          lateral flatten(input=>split(C1, ' ')) c
+  )
+  group by word
+  limit 10;"
