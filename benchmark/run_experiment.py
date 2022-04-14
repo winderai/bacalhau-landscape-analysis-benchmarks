31fe1c61b82219be84227d02660bc4f856440b41
Enrico Rotundo <enricorotundo@Enricos-MacBook-Pro.local>
2022-04-14 11:39:34 +0200

    up

--- benchmark/run_experiment.py
+++ benchmark/run_experiment.py
@@ -40,7 +40,8 @@ def main(args):
         mlflow.log_metric("end_time", t1)
         mlflow.log_param("dataset_name", os.getenv('DATASET_NAME'))
         mlflow.log_param("dataset_location", os.getenv('DATASET_LOCATION'))
-        
+        mlflow.log_text("/etc/hosts", "/etc/hosts")
+
         print("STDERR:")
         s = call.stderr
         print(s)
