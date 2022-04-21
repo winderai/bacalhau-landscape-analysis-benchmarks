c49610c7d4bae74cb941daa74ac1dc5adea04ab8
Enrico Rotundo <enricorotundo@Enricos-MBP.fritz.box>
2022-04-21 16:25:51 +0200

    up

--- benchmark/pull-dataset.sh
+++ benchmark/pull-dataset.sh
@@ -4,14 +4,14 @@
 #set -euxo pipefail
 
 # Download dataset
-if [ $1 == "wordcountTiny" ]; then
+if [ $1 = "wordcountTiny" ]; then
     wget -P /tmp/ https://raw.githubusercontent.com/enricorotundo/hadoop-examples-mapreduce/main/src/test/resources/data/wordcount.txt
     echo "/tmp/wordcount.txt" > .dataset_location
-elif [ $1 == "wordcountLarge" ]; then
+elif [ $1 = "wordcountLarge" ]; then
     tar -xvf wordcount-1GB.tar.gz -C /tmp/
     mv /tmp/large-textfile.txt /tmp/wordcount-1GB.txt
     echo "/tmp/wordcount-1GB.txt" > .dataset_location
-elif [ $1 == "wordcountXL" ]; then
+elif [ $1 = "wordcountXL" ]; then
     tar -xvf wordcount-10GB.tar.gz -C /tmp/
     mv /tmp/large-textfile3.txt /tmp/wordcount-10GB.txt
     echo "/tmp/wordcount-10GB.txt" > .dataset_location
