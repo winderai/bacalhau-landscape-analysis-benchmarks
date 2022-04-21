ad22c02c25d665ea14db06fd94901f964a232bc3
Enrico Rotundo <enricorotundo@Enricos-MBP.fritz.box>
2022-04-21 12:13:02 +0200

    up

--- benchmark/word-count/postgres/word-count.sh
+++ benchmark/word-count/postgres/word-count.sh
@@ -3,11 +3,8 @@
 # fail fast
 set -euxo pipefail
 
-if [ ${DATASET_NAME} = "wordcountTiny" ]; then
-    sudo -u postgres psql -d ${DB_NAME} -c "SELECT word, COUNT(word) 
-    FROM ${DATASET_NAME} 
-    GROUP BY word 
-    LIMIT 10;"
-else
-  echo "try again"
-fi
+
+sudo -u postgres psql -d ${DB_NAME} -c "SELECT word, COUNT(word) 
+FROM ${DATASET_NAME} 
+GROUP BY word 
+LIMIT 10;"
