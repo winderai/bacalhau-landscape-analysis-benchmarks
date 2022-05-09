7f12385b6cb3e2a64feea306c9707d3f25734662
Enrico Rotundo <enrico@winder.ai>
2022-05-09 12:22:57 +0200

    up

--- benchmark/word-count/hadoop/run.sh
+++ benchmark/word-count/hadoop/run.sh
@@ -4,4 +4,10 @@
 set -euxo pipefail
 
 # launch job
-$HADOOP_HOME/bin/yarn jar word-count/hadoop/wc.jar WordCount ${DATASET_LOCATION} out
+$HADOOP_HOME/bin/yarn jar word-count/hadoop/wc.jar WordCount ${DATASET_LOCATION} out \
+    -D yarn.app.mapreduce.am.resource.memory-mb=32000 \
+    -D yarn.app.mapreduce.am.resource.vcores=4 \
+    -D mapreduce.map.resource.memory-mb=96000 \
+    -D mapreduce.map.resource.vcores=32 \
+    -D mapreduce.reduce.resource.memory-mb=96000 \
+    -D mapreduce.reduce.resource.vcores=32
