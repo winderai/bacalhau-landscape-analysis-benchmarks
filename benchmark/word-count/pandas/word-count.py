7e46996a573c965037e719e0088f6faa21ea3355
Enrico Rotundo <enrico@winder.ai>
2022-05-17 07:56:25 +0000

    Update benchmark/word-count/pandas/word-count.py

--- benchmark/word-count/pandas/word-count.py
+++ benchmark/word-count/pandas/word-count.py
@@ -11,9 +11,6 @@ new_df = (
     .split(expand=True)
     .stack()
     .value_counts()
-    # .reset_index()
 )
-# new_df.columns = ['Word', 'Frequency'] 
-# new_df = new_df.sort_values(['Frequency', 'Word'], ascending=False)
 
 print(new_df.head())
