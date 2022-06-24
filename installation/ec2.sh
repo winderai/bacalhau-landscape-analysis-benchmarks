4e2ba144ce6b22d4551eb3495743d773ce74afca
Enrico Rotundo <enrico.rotundo@gmail.com>
2022-06-24 10:57:09 +0200

    Delete ec2.sh

--- installation/ec2.sh
+++
@@ -1,7 +0,0 @@
-# This script will iterate over all regions of AWS and list EC2 instances
-
-for region in `aws ec2 describe-regions --output text | cut -f4`
-do
-     echo -e "\nListing Instances in region:'$region'..."
-     aws ec2 describe-instances --query "Reservations[*].Instances[*].{IP:PublicIpAddress,ID:InstanceId,Type:InstanceType,State:State.Name,Name:Tags[0].Value}" --output=table --region $region
-done
